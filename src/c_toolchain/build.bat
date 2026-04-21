949c7c7b72525dc62095640eae021c6ae7a8ec0c
Purushottam Jha <j.purushottam@iitg.ac.in>
2026-04-21 21:53:28 +0530

    Generate bitstream directly on terminal

---
+++ src/c_toolchain/build.bat
@@ -0,0 +1,15 @@
+@echo off
+echo COMPILING RISC-V C CODE...
+riscv-none-elf-gcc -O2 -march=rv32imf -mabi=ilp32 -ffp-contract=off -ffreestanding -nostdlib -mcmodel=medany -Wl,--no-warn-rwx-segments -T link.ld start.S *.c -o program.elf
+
+echo EXTRACTING BINARY...
+riscv-none-elf-objcopy -O binary program.elf program.bin
+
+echo CONVERTING TO VERILOG HEX...
+python bin2hex.py program.bin imem.hex
+
+echo INSTALLING INTO WORKSPACE...
+copy imem.hex ..\imem.hex
+copy imem.hex ..\dmem.hex
+
+echo DONE!
